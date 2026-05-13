30c842341cf8259d1de97189ef4e590dddc86727
acquarum <edoardo980@gmail.com>
2026-05-13 10:26:46 +0200

    Add hyprland config files

---
+++ .config/hypr/config/programs.lua
@@ -0,0 +1,10 @@
+
+---------------------
+---- MY PROGRAMS ----
+---------------------
+
+-- Set programs that you use
+Terminal    = "alacritty"
+FileManager = "dolphin"
+Menu        = "hyprlauncher"
+
---
+++ .config/hypr/hyprland.lua
@@ -0,0 +1,11 @@
+
+require('config.monitors')
+require('config.programs')
+require('config.autostart')
+require('config.environment')
+require('config.permissions')
+require('config.aesthetics')
+require('config.misc')
+require('config.input')
+require('config.keybindings')
+require('config.window-rules')
