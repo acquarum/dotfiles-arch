5ec044be7206a31eea39193133ed3072fd599914
acquarum <edoardo980@gmail.com>
2026-05-13 16:53:17 +0200

    Fix xdg-desktop-portal-hyprland not crashing. Autostart hyprpolkitagent

--- .config/hypr/config/autostart.lua
+++ .config/hypr/config/autostart.lua
@@ -1,4 +1,3 @@
-
 -------------------
 ---- AUTOSTART ----
 -------------------
@@ -6,12 +5,8 @@
 -- See https://wiki.hypr.land/Configuring/Basics/Autostart/
 
 -- Autostart necessary processes (like notifications daemons, status bars, etc.)
--- Or execute your favorite apps at launch like this:
+-- Or execute your favorite apps at launch
 --
--- hl.on("hyprland.start", function () 
---   hl.exec_cmd(terminal)
---   hl.exec_cmd("nm-applet")
---   hl.exec_cmd("waybar & hyprpaper & firefox")
--- end)
---
-
+hl.on("hyprland.start", function()
+	hl.exec_cmd("systemctl --user start hyprpolkitagent")
+end)
