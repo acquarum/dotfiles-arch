583971c25e1aa5b6b7f67393b0fc7f68f2e0792d
acquarum <edoardo980@gmail.com>
2026-07-18 16:50:28 +0200

    Specify build directory in vimtex

--- .config/nvim/plugin/vimtex.lua
+++ .config/nvim/plugin/vimtex.lua
@@ -2,4 +2,7 @@ vim.pack.add {
   'https://github.com/lervag/vimtex',
 }
 
-vim.g.vimtex_view_method = "zathura"
+vim.g.vimtex_view_method = 'zathura'
+vim.g.vimtex_compiler_latexmk = {
+  aux_dir = './build',
+}
