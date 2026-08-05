88d113d27babcdae8bc94c34b8f6be38f0ce83fa
acquarum <edoardo980@gmail.com>
2026-08-05 18:27:48 +0200

    Update plugins and make neovim stop recognizing hyphenated words

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -5,7 +5,7 @@ vim.opt.relativenumber = true -- Set relative numbered lines (default: false)
 vim.opt.scrolloff = 10 -- Minimal number of screen lines to keep above and below the cursor (default: 0)
 vim.opt.sidescrolloff = 8 -- Minimal number of screen columns either side of cursor if wrap is `false` (default: 0)
 vim.opt.wrap = false -- Display lines as one long line (default: true)
-vim.opt.linebreak = true -- Companion to wrap, don"t split words (default: false)
+vim.opt.linebreak = true -- Companion to wrap, don't split words (default: false)
 vim.opt.mouse = 'a' -- Enable mouse mode (default: "")
 vim.opt.cursorline = true -- Highlight the current line (default: false)
 
@@ -26,13 +26,15 @@ vim.opt.signcolumn = 'yes' -- Keep signcolumn on by default (default: "auto")
 vim.opt.colorcolumn = '100' -- Show a highlighted column at pos. 100
 vim.opt.showmatch = true -- Show a highlighted column at pos. 100
 vim.opt.cmdheight = 1 -- More space in the Neovim command line for displaying messages (default: 1)
-vim.opt.completeopt = {'menuone','noselect'} -- Set completeopt to have a better completion experience (default: "menu,preview")
-vim.opt.showmode = false -- We don"t need to see things like -- INSERT -- anymore (default: true)
+vim.opt.completeopt = { 'menuone', 'noselect' } -- Set completeopt to have a better completion experience (default: "menu,preview")
+vim.opt.showmode = false -- We don't need to see things like -- INSERT -- anymore (default: true)
 vim.opt.pumheight = 10 -- Pop up menu height (default: 0)
 vim.opt.conceallevel = 0 -- So that `` is visible in markdown files (default: 1)
 
 local undodir = vim.fn.expand '~/.local/share/nvim/undodir'
-if vim.fn.isdirectory(undodir) == 0 then vim.fn.mkdir(undodir, 'p') end
+if vim.fn.isdirectory(undodir) == 0 then
+  vim.fn.mkdir(undodir, 'p')
+end
 vim.opt.backup = false -- Creates a backup file (default: false)
 vim.opt.writebackup = false -- If a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited (default: true)
 vim.opt.swapfile = false -- Creates a swapfile (default: true)
@@ -44,8 +46,7 @@ vim.opt.autowrite = false -- Do not auto save
 vim.opt.updatetime = 250 -- Decrease update time (default: 4000)
 vim.opt.timeoutlen = 300 -- Time to wait for a mapped sequence to complete (in milliseconds) (default: 1000)
 
-vim.opt.backspace = { 'indent','eol','start' } -- Allow backspace on (default: "indent,eol,start")
-vim.opt.iskeyword:append '-' -- Hyphenated words recognized by searches (default: does not include "-")
+vim.opt.backspace = { 'indent', 'eol', 'start' } -- Allow backspace on (default: "indent,eol,start")
 
 vim.schedule(function() vim.opt.clipboard:append 'unnamedplus' end)
 vim.opt.encoding = 'utf-8' -- The encoding written to a file (default: "utf-8")
